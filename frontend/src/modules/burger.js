8b344a8c0abbed03be0531dcb1c345f44ca3c455
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-04-04 15:08:19 +0300

    feat: fix comments

--- frontend/src/modules/burger.js
+++ frontend/src/modules/burger.js
@@ -1,7 +1,6 @@
 export class Burger {
   constructor(domElement) {
     domElement.addEventListener('click', (el) => {
-      // console.log('click on burger', domElement.dataset.target);
       document.querySelectorAll(`.${domElement.dataset.target}`).forEach((menuElement) => {
         menuElement.classList.toggle('menu_visible');
       });
